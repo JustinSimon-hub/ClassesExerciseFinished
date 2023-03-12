185bc79eb9528fcf2be3255d60ef671b41b78bf5
Justin Simon <justinsimon@Justins-MBP.lan>
2023-03-11 20:57:10 -0800

    Finished

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Classes
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Make = "Chevy";
+            car.Model = "Trax";
+            car.Year = 2020;
+            Console.WriteLine($"I got a {car.Year} {car.Make} {car.Model}");
+        }
+    }
+}
+
+
+
+
+/*
+## Exercise 1:
+
++ Create a class named Car - make sure it is public
+
+### In the Car class:
+   + Create a Make property of type string that is public
+   + Create a Model property of type string that is public
+   + Create a Year property of type int that is public
+### In the Main method:
+   + Create a new instance of the Car class
+   + Set values in the properties for the object
+   + Print the values of each property to the Console
+*/
